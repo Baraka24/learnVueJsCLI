e2de05812d6e53dd6d6aa00b24563e9a419ecc69
Baraka24 <bkinywa24@gmail.com>
2023-10-30 22:57:58 +0200

    Learn Vue Js CLI

--- timer-by-baraka/src/components/MyTimer.vue
+++ timer-by-baraka/src/components/MyTimer.vue
@@ -3,7 +3,7 @@
     <div id="clock">
         <div id="hours">{{ Math.floor((time/60)/60) }} HEURE</div>
         <div>:</div>
-        <div id="minutes">{{ Math.floor(time/60) }} MI</div>
+        <div id="minutes">{{ Math.floor(time/60) }} MIN</div>
         <div>:</div>
         <div id="seconds">{{ time }} S</div><br>
     </div>
