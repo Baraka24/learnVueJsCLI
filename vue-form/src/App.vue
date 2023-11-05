6725189452bc9c2e85a11972e47122ac30a64fa1
Baraka24 <bkinywa24@gmail.com>
2023-11-05 23:00:54 +0200

    Learn VueJsForm

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <VueForm>
+
+  </VueForm>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import VueForm from './components/VueForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueForm
   }
 }
 </script>
---
+++ vue-form/src/components/VueForm.vue
@@ -0,0 +1,8 @@
+<template>
+    <form>
+        <p>Add item</p>
+        <p>Item name: <input type="text" required></p>
+        <p>How many: <input type="number"></p>
+        <button type="submit">Add item</button>
+    </form>
+</template>
