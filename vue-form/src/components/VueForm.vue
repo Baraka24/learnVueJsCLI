ffab3e8f5baeff1d3c5f388f53e71319d172aa8c
Baraka24 <bkinywa24@gmail.com>
2023-11-07 22:19:00 +0200

    Learn VueJS Form

--- vue-form/src/components/VueForm.vue
+++ vue-form/src/components/VueForm.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-        <form v-on:submit.prevent="addItem">
+        <form @submit.prevent="addItem">
             <p>Add item</p>
-            <p>Item name: <input type="text" required></p>
-            <p>How many: <input type="number"></p>
+            <p>Item name: <input type="text" v-model="itemName" required></p>
+            <p>How many: <input type="number" v-model="itemNumber"></p>
             <button type="submit">Add item</button>
         </form>
         <p>Shopping list</p>
-        <ul v-for="item in shoppingList" v-bind:key="item">
-            <li>{{ item.name }}, {{ item.number }}</li>
+        <ul>
+            <li v-for="(item, index) in shoppingList" :key="index">
+                {{ item.name }}, {{ item.number }}
+            </li>
         </ul>
     </div>
 </template>
@@ -17,7 +19,7 @@
 export default {
     data() {
         return {
-            shoppingList : [],
+            shoppingList: [],
             itemName: null,
             itemNumber: null
         }
@@ -26,11 +28,11 @@ export default {
         addItem() {
             let item = {
                 name: this.itemName,
-                Number: this.itemNumber
+                number: this.itemNumber
             }
             this.shoppingList.push(item);
-            /* this.itemName = null
-            this.itemNumber = null */
+            this.itemName = null; // Reset the item name input
+            this.itemNumber = null; // Reset the item number input
         }
     }
 }
