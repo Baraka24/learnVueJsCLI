9e46116f5e6ae96772845e94b7985b1407af09bb
Baraka24 <bkinywa24@gmail.com>
2023-11-08 08:08:33 +0200

    Learn VueJs Checkboxes

---
+++ vue-form-checkbox/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <MyForm />
+</template>
+
+<script>
+import MyForm from './components/MyForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-form-checkbox/src/components/MyForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <!-- <center>
+            <input v-model="checked" type="checkbox" name="check" />
+            <label for="check">{{ checked }}</label>
+
+            <p>Tutorials:{{ tutorials }}</p>
+        </center> -->
+        <input v-model="tutorials" type="checkbox" value="Data Structures" name="ds">
+        <label for="ds">Data Structures</label>
+        <input v-model="tutorials" type="checkbox" value="Algoritms" name="al">
+        <label for="al">Algoritms</label>
+        <input v-model="tutorials" type="checkbox" value="Machine Learning" name="ml">
+        <label for="ml">Machine Learning</label>
+    </div>
+</template>
+
+<script>
+export default {
+data() {
+	return {
+	checked: '',
+	tutorials: [],
+	}
+},
+}
+</script>
