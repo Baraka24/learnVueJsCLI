98f00b13f67c47f2cbf56e9211864d828c9981cf
Baraka24 <bkinywa24@gmail.com>
2023-11-13 22:27:13 +0200

    Learn VueJs Form select

--- vue-form-select/src/components/MyForm.vue
+++ vue-form-select/src/components/MyForm.vue
@@ -1,3 +1,21 @@
 <template>
     <h1>My select</h1>
+    Programming language : {{ language }} <br>
+    <select v-model="language" multiple>
+        <option value="Java">Java</option>
+        <option value="C++">C++</option>
+        <option value="Python">Python</option>
+        <option value="Javascript">Javascript</option>
+    </select>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      category: '',
+      language: '',
+    }
+  },
+}
+</script>
