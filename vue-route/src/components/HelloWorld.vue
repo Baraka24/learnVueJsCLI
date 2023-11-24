6247726dfaad7e19ea9e6f230ceca148f02e2478
Baraka Kinywa <bkinywa24@gmail.com>
2023-11-24 23:17:20 +0200

    LEARN VUE JS ROUTER

--- vue-route/src/components/HelloWorld.vue
+++ vue-route/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="alert">
-    <strong>Web developer!</strong> Job.
+  <div class="alert" v-for="job in jobs" :key="job.id">
+    <strong>{{job.title}}</strong> Job.
+    <hr>
   </div>
 </template>
 
