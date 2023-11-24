147a5fc5cc3ec47f779f9eace49399447898966a
Baraka Kinywa <bkinywa24@gmail.com>
2023-11-25 01:02:15 +0200

    LEARN VUE JS ROUTER

--- vue-route/src/components/NotFound.vue
+++ vue-route/src/components/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="error-page">
-        <img src="https://i.stack.imgur.com/1q84L.png" alt="404 Page Not Found">
+        <img src="https://imgs.search.brave.com/pwudBgp90IvbB7s5BcbobXv3It38eqpsp6aJMcvgOLU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdC5k/ZXBvc2l0cGhvdG9z/LmNvbS8zMzM2MzM5/LzQ2MzMvaS82MDAv/ZGVwb3NpdHBob3Rv/c180NjMzMTI2NS1z/dG9jay1waG90by00/MDQtZXJyb3ItcGFn/ZS1jb25jZXB0LW9m/LmpwZw" alt="404 Page Not Found">
         <h1>Page Not Found</h1>
         <p>The page you are looking for could not be found.</p>
         <router-link to="/">Home</router-link>
